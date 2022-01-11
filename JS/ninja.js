35c1e61843433a3026eb33236cf6c6c44bc09c27
Craig Brinkman <craigfrancis77@gmail.com>
2022-01-11 13:02:55 -0700

    commmit

--- JS/ninja.js
+++ JS/ninja.js
@@ -7,12 +7,34 @@ class Ninja{
     }
 
     sayName(){
-        console.log('My name is ${this.name}');
+        console.log(`My name is ${this.name}`);
     }
     showStats(){
-        console.log('Ninja Stats. Health : ${this.health} Speed : ${this.speed} Strength : ${this.strength}')
+        console.log(`Name : ${this.name} Health : ${this.health} Speed : ${this.speed} Strength : ${this.strength}`)
     }
     drinkSake(){
         this.health += 10;
     }
 }
+
+class Sensei extends Ninja{
+    constructor(name, health){
+        super(name);
+        this.health = 200;
+        this.wisdom=10;
+        this.speed= 10;
+        this.strength= 10;
+        }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Wise Message")
+    }
+    }
+
+superSensei = new Sensei("Master")
+console.log(superSensei)
+superSensei.speakWisdom();
+superSensei.showStats();
+
+
