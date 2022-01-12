51a1d65f1b2085ad39ef45bf89189c54ca4b0ba5
Craig Brinkman <craigfrancis77@gmail.com>
2022-01-11 20:16:38 -0700

    commit

---
+++ JS/firstjsk/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+    <h2>Things I need to do.</h2>
+    <ul>
+      <li>Homework</li>
+      <li>Eat</li>
+      <li>Sleep</li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
